a352c0ace2d71aa42d54f140ff3a2515ed4c273e
Giuseppe <joseph.grant@mottach.superate.org.pa>
2022-04-09 22:50:54 -0500

    Version 1.5
    - New JS structure from Objects to get CSS attributes.
    - Eliminates the bg cube.

--- docs/js/mains.js
+++ docs/js/mains.js
@@ -1,65 +1,15 @@
-const key = [
-  {
-    id: 'green',
-    primary: '#abf4ce',
-    secondary: '#73edae',
-    tertiary: '#1f804e',
-    quaternary: '#384f44',
-  },
-  {
-    id:'purple',
-    primary: '#7707ff',
-    secondary: '#a473ed',
-    tertiary: '#4b1f80',
-    quaternary: '#3b384f',
-  },
-  {
-    id:'red',
-    primary: '#ff075a',
-    secondary: '#ed7373',
-    tertiary: '#801f1f',
-    quaternary: '#4f3838',
-  },
-  {
-    id:'yellow',
-    primary: '#f3ff07',
-    secondary: '#e7ed73',
-    tertiary: '#80801f',
-    quaternary: '#4f4e38',
-  },
-  {
-    id:'blue',
-    primary: '#7397ed',
-    secondary: '#89c5f5',
-    tertiary: '#1f4f80',
-    quaternary: '#38414f',
-  },
-  {
-    id:'pink',
-    primary: '#ee36b7',
-    secondary: '#ed73d7',
-    tertiary: '#801f70',
-    quaternary: '#4e384f',
-  },
-];
+const colors = document.querySelectorAll('.box');
+const sides = document.querySelectorAll('.side');
+const tops = document.querySelectorAll('.top');
+const ups = document.querySelectorAll('.up');
+const belows = document.querySelectorAll('.below');
 
-const colors = document.getElementsByClassName('box');
-let i;
-for (i = 0; i < colors.length; i++) {
-  colors[i].addEventListener('click', changecolor);
+function changeColor() {
+  let color = this.getAttribute('color');
+  sides.forEach(s => s.setAttribute('color',`${color}`));
+  tops.forEach(t => t.setAttribute('color',`${color}`));
+  ups.forEach(u => u.setAttribute('color',`${color}`));
+  belows.forEach(b => b.setAttribute('color',`${color}`));
 }
 
-function changecolor() {
-  let color = this.getAttribute('data-color');
-  const select = key.find(elemento => {return elemento.id === color;});
-
-  let root = document.querySelector(':root');
-  root.style.setProperty('--bg', select.quaternary);
-  root.style.setProperty('--side',`linear-gradient( ${select.primary}, ${select.secondary} )`);
-  root.style.setProperty('--top', select.tertiary);
-  root.style.setProperty('--up', select.primary);
-  root.style.setProperty('--below', select.secondary);
-  //   var rootStyles = getComputedStyle(root);
-  //   var mainColor = rootStyles.getPropertyValue('--bg');
-  //   console.log(rootStyles);
-}
+colors.forEach(c => c.addEventListener('click', changeColor));
